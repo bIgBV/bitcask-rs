51ab725ae7646b8e9f2d5b730282e3ccd9cb82e6
Bhargav Voleti <bhargav.voleti93@gmail.com>
2024-06-18 23:00:51 -0700

    Implement shitty get
    Horrendously bad, but it validates the idea. We can write and read
    arbitrary byte strings as key value pairs from a file on disk.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::{
     env,
     fs::{self, File},
-    io::{Seek, SeekFrom, Write},
+    io::{Read, Seek, SeekFrom, Write},
+    mem,
+    os::unix::fs::FileExt,
     time::SystemTime,
 };
 
 fn main() {
     let mut cask = Cask::new().unwrap();
-    cask.insert("hello", "world");
+    cask.insert("hello", "world 🧏‍♀️");
+    let result = cask.get("hello").unwrap();
+
+    let val = String::from_utf8(result).unwrap();
+    println!("{val}");
 }
 
 struct Cask {
@@ -50,6 +56,55 @@ impl Cask {
             .unwrap();
         self.file_handle.write_all(key).unwrap();
         self.file_handle.write_all(value).unwrap();
+    }
+
+    pub fn get<K>(&mut self, key: K) -> Option<Vec<u8>> {
+        let mut buf = [0u8; 8];
+        self.file_handle.read_at(&mut buf, 0).unwrap();
+        let timestamp = u64::from_le_bytes(buf);
+        println!("{timestamp}");
+
+        let mut buf = [0u8; 2];
+        self.file_handle
+            .read_at(&mut buf, mem::size_of::<u64>() as u64)
+            .unwrap();
+        let key_size = u16::from_le_bytes(buf);
+        dbg!(key_size);
+
+        let mut buf = [0u8; 4];
+        self.file_handle
+            .read_at(
+                &mut buf,
+                (mem::size_of::<u64>() + mem::size_of::<u16>()) as u64,
+            )
+            .unwrap();
+        let val_size = u32::from_le_bytes(buf);
+        dbg!(val_size);
+
+        let key_offset = mem::size_of::<u64>() + mem::size_of::<u16>() + mem::size_of::<u32>();
+        let mut buf = vec![0; key_size.into()];
+        self.file_handle
+            .seek(SeekFrom::Start(key_offset as u64))
+            .unwrap();
+
+        dbg!(self.file_handle.read_exact(&mut buf)).unwrap();
+        dbg!(&buf);
+
+        let key = String::from_utf8(buf).unwrap();
+        dbg!(key);
+
+        let val_offset = mem::size_of::<u64>()
+            + mem::size_of::<u16>()
+            + mem::size_of::<u32>()
+            + key_size as usize;
+        self.file_handle
+            .seek(SeekFrom::Start(val_offset as u64))
+            .unwrap();
+        let mut buf = vec![0; val_size.try_into().unwrap()];
+
+        dbg!(self.file_handle.read_exact(&mut buf).unwrap());
+
+        Some(buf)
     }
 }
 
@@ -100,19 +155,5 @@ where
         key_size: key_len as u16,
         value_size: val_len as u32,
         timestamp,
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use crate::encode;
-
-    #[test]
-    fn simple_encode() {
-        let key = "hello";
-        let val = "world";
-        let header = encode(key, val);
-
-        assert_eq!(header.key_size as usize, key.as_bytes().len())
     }
 }
